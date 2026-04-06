c3a493038d353db28d6633029f8da3e0eca37c0d
lauzarg <lauzarate@mi.unc.edu.ar>
2026-04-05 21:16:14 -0300

    se controla el flujo de inicio de sesion y el acceso a la pagina de bienvenidos

--- login-frontend/src/app/app.routes.ts
+++ login-frontend/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Login } from './login/login';
 import { Home } from './home/home';
 
 export const routes: Routes = [
-  { path: '', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
   { path: 'home', component: Home },
-  { path: '**', redirectTo: '' }
 ];
